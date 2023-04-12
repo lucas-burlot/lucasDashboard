92c8a11e31ae2e0eb86818a8a1b6ef7814bfb85e
Pewoh <burl410@gmail.com>
2023-04-12 11:54:30 +0200

    Add bootstrap5 + routes + users Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {UserSignUpComponent} from "./user/user-sign-up/user-sign-up.component";
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: 'sign-up', component: UserSignUpComponent}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
