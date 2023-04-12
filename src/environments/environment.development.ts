7124a86215a369dc324fa76fd3d58d31914ae23d
Pewoh <burl410@gmail.com>
2023-04-12 11:58:38 +0200

    test

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://ycmdbyvvpbbjjwwjsmve.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InljbWRieXZ2cGJiamp3d2pzbXZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODEyODM3MjMsImV4cCI6MTk5Njg1OTcyM30.U_pABmuTBaAVH2YvL9aJ_AD6LMYcAJsgz2USsKpYHis',
+};
