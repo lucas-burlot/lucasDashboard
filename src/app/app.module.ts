855e288043a02ebab0a01c4a056c13725e6eb269
Pewoh <burl410@gmail.com>
2023-04-12 12:06:40 +0200

    New component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {UserSignUpComponent} from "./user/user-sign-up/user-sign-up.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSignUpComponent,
+    UserSignUpComponent
   ],
   imports: [
     BrowserModule,
